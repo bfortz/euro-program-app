65c3ad9876adfff4da85e65a48a0a37c41f3f3d5
Bernard Fortz <bernard.fortz@gmail.com>
2022-07-11 15:36:19 +0200

    IFCS2022: Rooms

--- src/euro_program_app/ifcs2022.clj
+++ src/euro_program_app/ifcs2022.clj
@@ -17,19 +17,21 @@
    8 (ts "Wednesday 16:30-18:00" "W" "F")
    9 (ts "Thursday 9.00-10.00" "H" "A") 
    10 (ts "Thursday 10.00-10.30" "H" "B") 
-   11 (ts "Thursday 11.00-13.00" "H" "C") 
-   12 (ts "Thursday 14.30-15.30" "H" "D") 
-   13 (ts "Friday 9.00-10.30" "F" "A") 
-   14 (ts "Friday 11.00-12.00" "F" "B") 
-   15 (ts "Friday 12.00-13.00" "F" "C") 
-   16 (ts "Friday 14.30-15.30" "F" "D") 
-   17 (ts "Friday 15.30-16.00" "F" "E") 
-   18 (ts "Friday 16.30-18.00" "F" "F") 
-   19 (ts "Friday 18.00-19.30" "F" "G") 
-   20 (ts "Saturday 9.00-10.30" "S" "A") 
-   21 (ts "Saturday 11.00-12.00" "S" "B") 
-   22 (ts "Saturday 12.00-13.00" "S" "C") 
-   23 (ts "Saturday 13.00-13.30" "S" "D") 
+   11 (ts "Thursday 11.00-11.15" "H" "C") 
+   12 (ts "Thursday 11.15-12.00" "H" "D") 
+   13 (ts "Thursday 12.00-13.00" "H" "E") 
+   14 (ts "Thursday 14.30-15.30" "H" "F") 
+   15 (ts "Friday 9.00-10.30" "F" "A") 
+   16 (ts "Friday 11.00-12.00" "F" "B") 
+   17 (ts "Friday 12.00-13.00" "F" "C") 
+   18 (ts "Friday 14.30-15.30" "F" "D") 
+   19 (ts "Friday 15.30-16.00" "F" "E") 
+   20 (ts "Friday 16.30-18.00" "F" "F") 
+   21 (ts "Friday 18.00-19.30" "F" "G") 
+   22 (ts "Saturday 9.00-10.30" "S" "A") 
+   23 (ts "Saturday 11.00-12.00" "S" "B") 
+   24 (ts "Saturday 12.00-13.00" "S" "C") 
+   25 (ts "Saturday 13.00-13.30" "S" "D") 
    })
 
 (defrecord Stream [name sessions])
@@ -78,93 +80,99 @@
 (def sessions
   (into 
     (sorted-map) 
-    {1 (session "Analysis of Data Streams" 1 [] 1 [282] 1)
-     2 (session "Categorical Data Analysis and Visualization" 1 [] 2 [283] 1)
-     3 (session "Opening Session" 2 [] 3 [] 1)
-     4 (session "Keynote Dianne Cook" 2 [] 4 [256] 1)
-     5 (session "Symbolic Data Analysis 1" "SDA" [] 5 [157, 187, 275] 1)
-     6 (session "Functional Data Analysis 1" "FDA" [] 5 [47, 85, 235] 2)
-     7 (session "Social Network Analysis 1" "SNA" [] 5 [13, 38, 195] 3)
-     8 (session "50 Years of Biplots 1" "BIP" [] 5 [75, 158, 240] 4)
-     9 (session "Statistical Learning and Data Mining 1" "SLDM" [] 5 [99, 105, 144] 5)
-     10 (session "Clustering 1" "CL" [] 5 [50, 160, 237] 6)
-     11 (session "Robust Methods 1" "RM" [] 5 [138, 148, 232] 7)
-     12 (session "Model-based Clustering 1" "MBC" [] 5 [6, 18, 76] 8)
-     13 (session "Supervised Machine Learning 1" "S-ML" [] 5 [4, 98, 216] 9)
-     14 (session "Machine Learning 1" "ML" [] 5 [129, 154, 173] 10)
-     15 (session "Symbolic Data Analysis 2" "SDA" [] 6 [56, 59, 86] 1)
-     16 (session "Functional Data Analysis 2" "FDA" [] 6 [84, 135, 226] 2)
-     17 (session "Supervised Classification 1" "SC" [] 6 [126, 205, 230] 3)
-     18 (session "Statistics and Econometric Methods" "SEM" [] 6 [61, 97, 141] 4)
-     19 (session "Statistical Learning and Data Mining 2" "SLDM" [] 6 [27, 131, 133] 5)
-     20 (session "Clustering 2" "CL" [] 6 [10, 25, 184 ] 6)
-     21 (session "Robust Methods 2" "RM" [] 6 [90, 114, 132] 7)
-     22 (session "Model-based Clustering 2" "MBC" [] 6 [24, 55, 79] 8)
-     23 (session "Data Science in Social Sciences 1" "DS-SS" [] 6 [33, 91, 153] 9)
-     24 (session "Machine Learning 2" "ML" [] 6 [11, 172, 189] 10)
-     25 (session "Classification of Time Series" 3 [] 7 [81, 149, 167] 1)     
-     26 (session "Benchmarking Challenge" 3 [] 7 [264, 265, 268, 273] 2)
-     27 (session "Dimension Reduction" "DR" [] 8 [71, 102, 214, 219] 1)
-     28 (session "Functional Data Analysis 3" "FDA" [] 8 [21, 47, 152, 177] 2)
-     29 (session "Time Series" "TS" [] 8 [7, 109, 164, 258] 3) 
-     30 (session "Supervised Classification - Trees" "SCT" [] 8 [82, 210, 239, 242] 4)
-     31 (session "Data Science in Health Sciences" "DS-HS" [] 8 [31, 125, 254, 255] 5)
-     32 (session "Clustering 3" "CL" [] 8 [34, 37, 41] 6)
-     33 (session "Compositional Data Analysis 1" "CoDA" [] 8 [14, 44, 201] 7)
-     34 (session "Model-based Clustering 3" "MBC" [] 8 [28, 32, 257] 8)
-     35 (session "Text Mining" "TM" [] 8 [30, 48, 168, 171] 9)
-     36 (session "Symbolic Data Analysis 3" "SDA" [] 9 [107, 119, 209] 1)
-     37 (session "Functional Data Analysis 4" "FDA" [] 9 [78, 155, 212] 2)
-     38 (session "Optimization in Classification and Clustering 1" "OCC" [] 9 [77, 127, 231] 3)
-     39 (session "50 Years of Biplots 2" "BIP" [] 9 [96, 145, 222] 4)
-     40 (session "Data Science in Economics and Finance 1" "DS-EF" [] 9 [121, 139, 185] 5)
-     41 (session "Clustering 4" "CL" [] 9 [29, 192, 221] 6)
-     42 (session "Compositional Data Analysis 2" "CoDA" [] 9 [170, 174] 7)
-     43 (session "Model-based Clustering 4" "MBC" [] 9 [19, 123, 204] 8)
-     44 (session "Data Science in Social Sciences 2" "DS-SS" [] 9 [49, 113, 180] 9)
-     45 (session "Machine Learning 3" "ML" [] 9 [2, 112, 270] 10)
-     46 (session "Poster Session 1" 4 [] 10 [] 1)
-     47 (session "Awards" 2 [] 11 [] 1)
-     48 (session "Keynote Charles Bouveyron" 2 [] 12 [272] 1)
-     49 (session "COVID Data Analysis" 3 [] 13 [206, 260, 262] 1)
-     50 (session "Categorical Data Analysis and Visualization" 3 [] 13 [100, 117, 151] 2)
-     51 (session "Presidential Address Angela Montanari" 2 [] 14 [284] 1)
-     52 (session "Symbolic Data Analysis 4" "SDA" [] 15 [108, 147, 186] 1)
-     53 (session "Functional Data Analysis 5" "FDA" [] 15 [43, 74, 134] 2)
-     54 (session "Interpretable Machine Learning" "IML" [] 15 [190, 193, 250] 3)
-     55 (session "50 Years of Biplots 3" "BIP" [] 15 [83, 103, 251] 4)
-     56 (session "Optimization in Classification and Clustering 2" "OCC" [] 15 [15, 143, 188] 5)
-     57 (session "Clustering 5" "CL" [] 15 [64, 95, 225] 6)
-     58 (session "Robust Methods 3" "RM" [] 15 [5, 208, 217] 7)
-     59 (session "Model-based Clustering 5" "MBC" [] 15 [26, 93, 198] 8)
-     60 (session "Spatial-Temporal Data Analysis 1" "STDA" [] 15 [122, 181, 267] 9)
-     61 (session  "SPE" "SPE" [] 15 [128, 130, 211] 10)
-     62 (session "Keynote Genevera Allen" 2 [] 16 [271] 1)
-     63 (session "Poster Session 2" 4 [] 17 [] 1)
-     64 (session "Data Science for Business and Marketing" "DS-BM" [] 18 [39, 51, 159, 163] 1)
-     65 (session "Functional Data Analysis 6" "FDA" [] 18 [65, 169, 202, 253, 280] 2)
-     66 (session "Social Network Analysis 2" "SNA" [] 18 [36, 224, 243, 245] 3)
-     67 (session "Data Science in Education" "DS-E" [] 18 [156, 241, 247, 248] 4)
-     68 (session "Statistical Learning and Data Mining 3" "SLDM" [] 18 [72, 87, 89, 146] 5)
-     69 (session "Clustering 6" "CL" [] 18 [16, 17, 23] 6)
-     70 (session "Robust Methods 4" "RM" [] 18 [42, 94, 116, 223] 7)
-     71 (session "Model-based Clustering 6" "MBC" [] 18 [8, 12, 110] 8)
-     72 (session "Time Series Classification" "TSC" [] 18 [1, 22, 118, 227] 9)
-     73 (session "IFCS Council meeting" 5 [] 19 [] 1)
-     74 (session "Dimension Reduction" 3 [] 20 [200, 203, 220] 1)
-     75 (session "Explainable Machine Learning" 3 [] 20 [259, 274, 279] 2)
-     76 (session "Dimension Reduction & Clustering" "DR+Cl" [] 21 [63, 66, 166] 1)
-     77 (session "Functional Data Analysis 7" "FDA" [] 21 [92, 124, 263] 2)
-     78 (session "Supervised Classification 2" "SC" [] 21 [104, 115, 175] 3)
-     79 (session "Model-based Clustering for Time Series" "MBC-TS" [] 21 [70, 136, 179] 4)
-     80 (session "Optimization in Classification and Clustering 3" "OCC" [] 21 [182, 199, 276] 5)
-     81 (session "Data Science in Economics and Finance 2" "DS-EF" [] 21 [35, 162, 197] 6)
-     82 (session "Data Science in Biology" "DS-B" [] 21 [111, 161, 238] 7)
-     83 (session "Spatial-Temporal Data Analysis 2" "STDA" [] 21 [69, 120, 142] 9)
-     84 (session "Machine Learning 4" "ML" [] 21 [40, 191, 196] 10)
-     86 (session "Keynote João Gama" 2 [] 22 [62] 1)
-     87 (session "Closing Session" 2 [] 23 [] 1)
-     }))
+    (zipmap 
+      (map inc (range)) 
+      (sort-by #(+ (* 1000 (:timeslot %)) (:track %)) 
+               (map #(apply session %) 
+                    [["Analysis of Data Streams" 1 [] 1 [282] 1]
+                     ["Categorical Data Analysis and Visualization" 1 [] 2 [283] 1]
+                     ["Opening Session" 2 [] 3 [] 0]
+                     ["Keynote Dianne Cook" 2 [] 4 [256] 0]
+                     ["Symbolic Data Analysis 1" "SDA" [] 5 [157, 187, 275] 5]
+                     ["Functional Data Analysis 1" "FDA" [] 5 [47, 85, 235] 2]
+                     ["Social Network Analysis 1" "SNA" [] 5 [13, 38, 195] 3]
+                     ["50 Years of Biplots 1" "BIP" [] 5 [75, 158, 240] 4]
+                     ["Statistical Learning and Data Mining 1" "SLDM" [] 5 [99, 105, 144] 8]
+                     ["Clustering 1" "CL" [] 5 [50, 160, 237] 6]
+                     ["Robust Methods 1" "RM" [] 5 [138, 148, 232] 7]
+                     ["Model-based Clustering 1" "MBC" [] 5 [6, 18, 76] 1]
+                     ["Supervised Machine Learning" "S-ML" [] 5 [40, 98] 9]
+                     ["Machine Learning 1" "ML" [] 5 [129, 154, 173] 10]
+                     ["Symbolic Data Analysis 2" "SDA" [] 6 [56, 59, 86] 5]
+                     ["Functional Data Analysis 2" "FDA" [] 6 [84, 135, 226] 2]
+                     ["Supervised Classification 1" "SC" [] 6 [126, 205, 230] 4]
+                     ["Statistics and Econometric Methods" "SEM" [] 6 [61, 97, 141] 9]
+                     ["Statistical Learning and Data Mining 2" "SLDM" [] 6 [27, 131, 133] 8]
+                     ["Clustering 2" "CL" [] 6 [10, 25, 184 ] 6]
+                     ["Robust Methods 2" "RM" [] 6 [90, 114, 132] 7]
+                     ["Model-based Clustering 2" "MBC" [] 6 [24, 55, 79] 1]
+                     ["Data Science in Social Sciences 1" "DS-SS" [] 6 [33, 91, 153] 3]
+                     ["Machine Learning 2" "ML" [] 6 [11, 172, 189] 10]
+                     ["Classification of Time Series" 3 [] 7 [81, 149, 167] 1]     
+                     ["Benchmarking Challenge" 3 [] 7 [264, 265, 268, 273] 10]
+                     ["Dimension Reduction" "DR" [] 8 [71, 102, 214, 219] 10]
+                     ["Functional Data Analysis 3" "FDA" [] 8 [21, 47, 152, 177] 2]
+                     ["Time Series" "TS" [] 8 [7, 109, 164, 258] 5] 
+                     ["Supervised Classification - Trees" "SCT" [] 8 [82, 210, 239, 242] 4]
+                     ["Data Science in Health Sciences" "DS-HS" [] 8 [31, 125, 254, 255] 8]
+                     ["Clustering 3" "CL" [] 8 [34, 37, 41] 6]
+                     ["Compositional Data Analysis 1" "CoDA" [] 8 [14, 44, 201] 9]
+                     ["Model-based Clustering 3" "MBC" [] 8 [28, 257, 198, 32] 1]
+                     ["Text Mining" "TM" [] 8 [30, 48, 168, 171] 3]
+                     ["Symbolic Data Analysis 3" "SDA" [] 9 [107, 119, 209] 5]
+                     ["Functional Data Analysis 4" "FDA" [] 9 [78, 155, 212] 2]
+                     ["Optimization in Classification and Clustering 1" "OCC" [] 9 [77, 127, 231] 8]
+                     ["50 Years of Biplots 2" "BIP" [] 9 [96, 145, 222] 4]
+                     ["Data Science in Economics and Finance 1" "DS-EF" [] 9 [121, 139, 185] 7]
+                     ["Clustering 4" "CL" [] 9 [29, 192, 221] 6]
+                     ["Compositional Data Analysis 2" "CoDA" [] 9 [170, 174] 9]
+                     ["Model-based Clustering 4" "MBC" [] 9 [19, 123, 204] 1]
+                     ["Data Science in Social Sciences 2" "DS-SS" [] 9 [49, 113, 180] 3]
+                     ["Machine Learning 3" "ML" [] 9 [2, 112, 270] 10]
+                     ["Poster Session 1" 4 [] 10 [45, 54, 57, 58, 67, 73, 88, 106, 137, 140, 176, 178, 183, 218] 1]
+                     ["Benchmarking Challenge Award" 2 [] 11 [] 0]
+                     ["IFCS Awards" 2 [] 12 [] 0]
+                     ["Keynote Edwin Diday" 2 [] 13 [281] 0]
+                     ["Keynote Charles Bouveyron" 2 [] 14 [272] 0]
+                     ["COVID Data Analysis" 3 [] 15 [206, 260, 262] 1]
+                     ["Categorical Data Analysis and Visualization" 3 [] 15 [100, 117, 151] 10]
+                     ["Presidential Address Angela Montanari" 2 [] 16 [284] 0]
+                     ["Symbolic Data Analysis 4" "SDA" [] 17 [108, 147, 186] 5]
+                     ["Functional Data Analysis 5" "FDA" [] 17 [43, 74, 134] 2]
+                     ["Interpretable Machine Learning" "IML" [] 17 [190, 193, 250] 10]
+                     ["50 Years of Biplots 3" "BIP" [] 17 [83, 103, 251] 4]
+                     ["Optimization in Classification and Clustering 2" "OCC" [] 17 [15, 143, 188] 8]
+                     ["Clustering 5" "CL" [] 17 [64, 95, 225] 6]
+                     ["Robust Methods 3" "RM" [] 17 [5, 208, 217] 7]
+                     ["Model-based Clustering 5" "MBC" [] 17 [26, 93] 1]
+                     ["Spatial-Temporal Data Analysis 1" "STDA" [] 17 [122, 181, 267] 3]
+                     ["SPE" "SPE" [] 17 [128, 130, 211] 9]
+                     ["Keynote Genevera Allen" 2 [] 18 [271] 0]
+                     ["Poster Session 2" 4 [] 19 [68, 101, 194, 213, 215, 216, 233, 234, 244, 249, 252, 269, 277, 278] 1]
+                     ["Data Science for Business and Marketing" "DS-BM" [] 20 [39, 51, 159, 163] 4]
+                     ["Functional Data Analysis 6" "FDA" [] 20 [65, 169, 202, 253, 280] 2]
+                     ["Social Network Analysis 2" "SNA" [] 20 [36, 224, 243, 245] 3]
+                     ["Data Science in Education" "DS-E" [] 20 [156, 241, 247, 248] 9]
+                     ["Statistical Learning and Data Mining 3" "SLDM" [] 20 [72, 87, 89, 146] 8]
+                     ["Clustering 6" "CL" [] 20 [16, 17, 23] 6]
+                     ["Robust Methods 4" "RM" [] 20 [42, 94, 116, 223] 7]
+                     ["Model-based Clustering 6" "MBC" [] 20 [8, 12, 110] 1]
+                     ["Time Series Classification" "TSC" [] 20 [1, 22, 118, 227] 10]
+                     ["IFCS Council meeting" 5 [] 21 [] 11]
+                     ["Dimension Reduction" 3 [] 22 [200, 203, 220] 1]
+                     ["Explainable Machine Learning" 3 [] 22 [259, 274, 279] 10]
+                     ["Dimension Reduction & Clustering" "DR+Cl" [] 23 [63, 66, 166] 6]
+                     ["Functional Data Analysis 7" "FDA" [] 23 [92, 124, 263] 2]
+                     ["Supervised Classification 2" "SC" [] 23 [104, 115, 175] 4]
+                     ["Model-based Clustering for Time Series" "MBC-TS" [] 23 [70, 136, 179] 4]
+                     ["Optimization in Classification and Clustering 3" "OCC" [] 23 [182, 199, 276] 8]
+                     ["Data Science in Economics and Finance 2" "DS-EF" [] 23 [35, 162, 197] 7]
+                     ["Data Science in Biology" "DS-B" [] 23 [111, 161, 238] 9]
+                     ["Spatial-Temporal Data Analysis 2" "STDA" [] 23 [69, 120, 142] 3]
+                     ["Machine Learning 4" "ML" [] 23 [4, 191, 196] 10]
+                     ["Keynote João Gama" 2 [] 24 [62] 0]
+                     ["Closing Session" 2 [] 25 [] 0]
+                     ])))))
 
 (def timeslots
   (reduce (fn [acc [id s]] (update-in acc [(:timeslot s) :sessions] #(conj % id))) timeslots sessions))
@@ -226,7 +234,7 @@
   (map #(update %1 :authors (fn [authors] (map (fn [k] (get user->id k)) authors))) abstracts))
 
 (defn add-session-to-users [acc a]
-  (reduce #(update %1 %2 (fn [s] (conj s (:session a)))) acc (:authors a)))
+  (reduce #(update %1 %2 (fn [s] (distinct (conj s (:session a))))) acc (:authors a)))
 
 (def user-sessions
   (reduce add-session-to-users {} abstracts))
@@ -241,9 +249,14 @@
 
 (def papers (zipmap (map :id abstracts) abstracts))
 
-(defrecord Program [timeslots streams sessions papers keywords users])
+(defrecord Room [room])
 
-(def program (Program. timeslots streams sessions papers keywords users))
+(def rooms 
+  (zipmap (range) (map #(into {} (Room. %)) ["Grand Hall" "118" "113" "213" "260" "115" "135" "159" "211" "215" "218" "Council Room"])))
+
+(defrecord Program [timeslots streams sessions papers keywords users rooms])
+
+(def program (Program. timeslots streams sessions papers keywords users rooms))
 
 (spit "resources/public/ifcs2022.edn" (into {} program))
 
